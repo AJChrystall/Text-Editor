9a06715a3ec7b8b9a95dea3af86249eb38322c88
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-07-06 13:24:24 +1000

    updated install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Listen for the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default installation prompt
+  event.preventDefault();
+  // Store the event to trigger the installation later
+  deferredPrompt = event;
+  // Show the Install button
+  butInstall.style.display = 'block';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Add a click event handler for the Install button
+butInstall.addEventListener('click', async () => {
+  // Check if the installation prompt is available
+  if (deferredPrompt) {
+    // Show the installation prompt
+    deferredPrompt.prompt();
+    // Wait for the user's response
+    const choiceResult = await deferredPrompt.userChoice;
+    // Reset the deferredPrompt variable
+    deferredPrompt = null;
+
+    // Log the user's choice
+    console.log('User choice:', choiceResult.outcome);
+  }
+});
+
+// Listen for the appinstalled event
+window.addEventListener('appinstalled', (event) => {
+  // Hide the Install button after the app is installed
+  butInstall.style.display = 'none';
+});
