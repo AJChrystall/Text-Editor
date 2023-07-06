49d362ef92592d682def1ecc824402661fecfd4f
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-07-06 15:43:49 +1000

    fixed added the src asset catching

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -1,8 +1,3 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
-
 const main = document.querySelector('#main');
 main.innerHTML = '';
 
@@ -10,18 +5,14 @@ const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
   spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
+    <div class="loading-container">
+      <div class="loading-spinner"></div>
+    </div>
   `;
   main.appendChild(spinner);
 };
 
 const editor = new Editor();
-
-if (typeof editor === 'undefined') {
-  loadSpinner();
-}
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
@@ -31,3 +22,4 @@ if ('serviceWorker' in navigator) {
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
